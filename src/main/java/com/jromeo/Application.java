55cfe0a442fe4b09e4fa96774280319cd2ee5f1f
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2024-03-18 19:41:23 +0100

    Menu + Application logic applied

--- src/main/java/com/jromeo/Application.java
+++ src/main/java/com/jromeo/Application.java
@@ -1,45 +1,196 @@
-package com.jromeo;
+import com.jromeo.utils.Menu;
+
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
-        /**
-         * Johan making sure that his methods are working
-         */
-        //        adminApi.adminLogin(
-//                new LoginDto("j@mail.com", "password")
-//        );
-//        adminApi.userRegister(
-//                new RegisterDto(
-//                        "Eiemi",
-//                        "asd",
-//                        "cc@mail.com",
-//                        "password"
-//                )
-//        );
+        while (!exit) {
+            // Display main menu
+            printMainMenu();
 
-//        adminApi.updateUserByEmail(
-//                new LoginDto("j@mail.com", "password"),
-//                "a@mail.com",
-//                new RegisterDto(
-//                        "Satan",
-//                        "Romeo",
-//                        "1337@hell.now",
-//                        "ragata"
-//                ));
+            // Get user choice
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
 
-//        adminApi.deleteUserByEmail(
-//                new LoginDto("j@mail.com", "password"),
-//                "1337@hell.now"
-//        );
+            // Process user choice
+            switch (choice) {
+                case 1:
+                    authMenu(scanner);
+                    break;
+                case 2:
+                    adminMenu(scanner);
+                    break;
+                case 3:
+                    studentMenu(scanner);
+                    break;
+                case 4:
+                    courseMenu(scanner);
+                    break;
+                case 5:
+                    userMenu(scanner);
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 6.");
+            }
+        }
 
-//        adminApi.updateToAdminRole(
-//                new LoginDto("a@mail.com", "password"),
-//                new EmailDto("cc@mail.com"));
+        System.out.println("Exiting application. Goodbye!");
+    }
 
-        /**
-         * Johan ends here, bye
-         */
+    public static void printMainMenu() {
+        System.out.println("********************");
+        System.out.println("*     Main Menu    *");
+        System.out.println("********************");
+        System.out.println("1. Auth Menu");
+        System.out.println("2. Admin Menu");
+        System.out.println("3. Student Menu");
+        System.out.println("4. Course Menu");
+        System.out.println("5. User Menu");
+        System.out.println("6. Exit");
+        System.out.print("Enter your choice: ");
+    }
 
+    public static void authMenu(Scanner scanner) {
+        boolean exit = false;
+
+        while (!exit) {
+            Menu.printAuthMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    // Call login method
+                    System.out.println("Performing login...");
+                    break;
+                case 2:
+                    // Call register method
+                    System.out.println("Performing registration...");
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 3.");
+            }
+        }
+    }
+
+    public static void adminMenu(Scanner scanner) {
+        boolean exit = false;
+
+        while (!exit) {
+            Menu.printAdminMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    // Call update user method
+                    System.out.println("Performing update user...");
+                    break;
+                case 2:
+                    // Call delete user method
+                    System.out.println("Performing delete user...");
+                    break;
+                case 3:
+                    // Call update user to admin method
+                    System.out.println("Performing update user to admin...");
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+            }
+        }
+    }
+
+    public static void studentMenu(Scanner scanner) {
+        boolean exit = false;
+
+        while (!exit) {
+            Menu.printStudentMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    // Call update student method
+                    System.out.println("Performing update student...");
+                    break;
+                case 2:
+                    // Call delete student method
+                    System.out.println("Performing delete student...");
+                    break;
+                case 3:
+                    // Call add course method
+                    System.out.println("Performing add course...");
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+            }
+        }
+    }
+
+    public static void courseMenu(Scanner scanner) {
+        boolean exit = false;
+
+        while (!exit) {
+            Menu.printCourseMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    // Call add course method
+                    System.out.println("Performing add course...");
+                    break;
+                case 2:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 2.");
+            }
+        }
+    }
+
+    public static void userMenu(Scanner scanner) {
+        boolean exit = false;
+
+        while (!exit) {
+            Menu.printUserMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    // Call view profile method
+                    System.out.println("Performing view profile...");
+                    break;
+                case 2:
+                    // Call update profile method
+                    System.out.println("Performing update profile...");
+                    break;
+                case 3:
+                    // Call change password method
+                    System.out.println("Performing change password...");
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+            }
+        }
     }
 }
--- src/main/java/com/jromeo/utils/Menu.java
+++ src/main/java/com/jromeo/utils/Menu.java
@@ -1,4 +1,54 @@
 package com.jromeo.utils;
 
 public class Menu {
+
+
+
+    public static void printAuthMenu() {
+        System.out.println("**********************");
+        System.out.println("*      Auth Menu     *");
+        System.out.println("**********************");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+    }
+
+    public static void printAdminMenu() {
+        System.out.println("**********************");
+        System.out.println("*     Admin Menu     *");
+        System.out.println("**********************");
+        System.out.println("1. Update User");
+        System.out.println("2. Delete User");
+        System.out.println("3. Update User to Admin");
+        System.out.println("4. Exit");
+    }
+
+    public static void printStudentMenu() {
+        System.out.println("**********************");
+        System.out.println("*    Student Menu    *");
+        System.out.println("**********************");
+        System.out.println("1. Update Student");
+        System.out.println("2. Delete Student");
+        System.out.println("3. Add Course");
+        System.out.println("4. Exit");
+    }
+
+    public static void printCourseMenu() {
+        System.out.println("**********************");
+        System.out.println("*    Course Menu     *");
+        System.out.println("**********************");
+        System.out.println("1. Add Course");
+        System.out.println("2. Exit");
+    }
+
+    public static void printUserMenu() {
+        System.out.println("**********************");
+        System.out.println("*      User Menu     *");
+        System.out.println("**********************");
+        System.out.println("1. View Profile");
+        System.out.println("2. Update Profile");
+        System.out.println("3. Change Password");
+        System.out.println("4. Exit");
+    }
 }
+
